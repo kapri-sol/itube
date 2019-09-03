de50be32097065cb9bfce41156d3a13213c22fdd
kapri-sol <ksm6902@gmail.com>
2019-09-03 19:26:42 +0900

    API and Schema Structue

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,6 @@ const appOptions: Options = {
   endpoint: GRAPHQL_ENDPOINT
 };
 
-const handleAppStart = () => console.log("Listening on port ${PORT}");
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
 app.start(appOptions, handleAppStart);
